f2dbcf5c9e9b41ae28f854c2b1d4d0d57775f4c9
Eric Berger <eric@harps.ch>
2020-06-19 10:50:12 +0200

    feat: adding authentifier exported

---
+++ src/authentifier.ts
@@ -0,0 +1,31 @@
+import * as Http from 'request-promise';
+
+export class Authentifier {
+  public static async getToken(opt: { url: string; client: string; secret: string; scope: string; }): Promise<string> {
+    if (!Authentifier._cached) {Authentifier._cached = {}; }
+    if (!Authentifier._cached[opt.client] || Authentifier._cached[opt.client].expires <= new Date().getTime()) {
+      const body = {
+        /* eslint-disable @typescript-eslint/camelcase */
+        client_id: opt.client,
+        client_secret: opt.secret,
+        grant_type: 'client_credentials',
+        scope: opt.scope,
+        /* eslint-enable @typescript-eslint/camelcase */
+      };
+      const resp = await Http.post(opt.url, {
+        form: body,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+      const respObj = JSON.parse(resp);
+      // console.info(respObj);
+      Authentifier._cached[opt.client] = {
+        expires: new Date().getTime() + respObj.expires_in * 1000 - 5000,
+        token: respObj.access_token,
+      };
+    }
+    return Authentifier._cached[opt.client].token;
+  }
+  private static _cached: {[key: string]: {token: string; expires: number }};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './api';
-export * from './models/db/iam-db'
+export * from './models/db/iam-db';
+export * from './authentifier';
