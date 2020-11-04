f5446cd3fb6580098c3b60c84985814b915d366e
Eric Berger <eric@harps.ch>
2020-11-04 10:54:59 +0100

    -

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -30,8 +30,8 @@ export class UsersController extends ControllerBase {
     // Since here, the user is considered as authorized
     const entity = new EntityUser();
 
-    if (req.query.filter && typeof req.query.filter === 'string')
-      entity.setFilter(req.query.filter);
+    // if (req.query.filter && typeof req.query.filter === 'string')
+    entity.setFilter(req.query.filter as any);
     
     if (req.query.include && (typeof req.query.include === 'string' || (Array.isArray(req.query.include))))
       entity.setIncludes(req.query.include);
